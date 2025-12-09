c158d981e6f5521cdea493c9b475ae088735abaf
Asta Halkjær From <andro.from@gmail.com>
2025-12-09 15:24:27 +0100

    Stronger counterexample (no global modality).

--- Counter_Example_PIL.thy
+++ Counter_Example_PIL.thy
@@ -24,7 +24,7 @@ lemma Pss_compl: \<open>X \<in> Pss \<Longrightarrow> - X \<in> Pss\<close>
   unfolding Pss_def by blast
 
 definition my_gframe :: \<open>int gframe\<close> where
-  \<open>my_gframe \<equiv> \<lparr> \<W> = UNIV, \<R> = \<lambda>x. {}, \<Pi> = Pss \<rparr>\<close>
+  \<open>my_gframe \<equiv> \<lparr> \<W> = UNIV, \<R> = \<lambda>x. UNIV, \<Pi> = Pss \<rparr>\<close>
 
 lemma wf_frame_mygframe: \<open>wf_frame (frame.truncate my_gframe)\<close>
   unfolding wf_frame_def unfolds my_gframe_def by blast  
@@ -53,12 +53,13 @@ text \<open>Nowhere-or-twice says that if formula p holds somewhere, then it hol
 text \<open>(We ignore de Bruijn complications and only instantiate with closed formulas.)\<close>
 
 abbreviation \<open>nowhere_or_twice p \<equiv>
-  \<^bold>\<down> (\<^bold>E (\<^bold>\<down>(\<^bold>@ (\<^bold>#0) p))) \<^bold>\<longrightarrow>
-  \<^bold>E (\<^bold>\<down> (\<^bold>E (\<^bold>\<down> (
-    (\<^bold>@ (\<^bold>#1) p) \<^bold>\<and>
-    (\<^bold>@ (\<^bold>#0) p) \<^bold>\<and>
-    \<^bold>\<not> (\<^bold>@ (\<^bold>#0) (\<^bold>\<bullet>(\<^bold>#1)))
-  ))))\<close>
+  \<^bold>@ (\<^bold>\<circle>0) (\<^bold>\<diamond> p) \<^bold>\<longrightarrow>
+  \<^bold>@ (\<^bold>\<circle>0) (\<^bold>\<diamond> (\<^bold>\<down> (
+    \<^bold>@ (\<^bold>\<circle>0) (\<^bold>\<diamond> (\<^bold>\<down> (
+      (\<^bold>@ (\<^bold>#1) p) \<^bold>\<and>
+      (\<^bold>@ (\<^bold>#0) p) \<^bold>\<and>
+      \<^bold>\<not> (\<^bold>@ (\<^bold>#0) (\<^bold>\<bullet>(\<^bold>#1)))
+  ))))))\<close>
 
 text \<open>Finite unions of arithmetic progressions are either empty or infinite.\<close>
 
@@ -98,5 +99,6 @@ theorem counter:
   shows \<open>\<not> (my_model, x) \<Turnstile> \<^bold>\<forall> (nowhere_or_twice (\<^bold>\<cdot>(\<^bold>#0))) \<^bold>\<longrightarrow> nowhere_or_twice (singleton y)\<close>
   using nowhere_or_twice_admissible not_nowhere_or_twice_singleton
   by (meson semantics.simps(3,4))
+
 
 end
