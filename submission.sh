4f671c0d1fd2f1d3bf0a3ed10a659dc403203a66
andsch <andsch@cs.aau.dk>
2026-07-10 13:32:47 +0200

    submission root file

--- submission.sh
+++ submission.sh
@@ -7,6 +7,7 @@ cp Derivational_Consistency.thy Q0_completeness
 cp Model_Existence.thy Q0_completeness
 cp Q0_Completeness.thy Q0_completeness
 cp README_submission.txt Q0_completeness/README.txt
+cp ROOT_submission Q0_completeness/ROOT
 
 zip -rXq Q0_completeness.zip Q0_completeness
 
